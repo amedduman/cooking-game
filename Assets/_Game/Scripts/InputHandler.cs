1a881b93e99758ceedeec71f2e2434a5efcb137a
amedduman <amedduman@protonmail.com>
2023-02-24 02:44:14 +0300

    Add wall sliding for player controller

--- Assets/_Game/Scripts/InputHandler.cs
+++ Assets/_Game/Scripts/InputHandler.cs
@@ -15,22 +15,7 @@ public class InputHandler : MonoBehaviour
 
     public Vector2 GetMovementVectorNormalized()
     {
-         // inputVector = Vector2.zero;
          Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
-        // if (Input.GetKey(KeyCode.W))
-        // {
-        //     inputVector.y = 1;
-        // }
-        // if (Input.GetKey(KeyCode.S))
-        // {
-        //     inputVector.y = -1;
-        // }if (Input.GetKey(KeyCode.A))
-        // {
-        //     inputVector.x = -1;
-        // }if (Input.GetKey(KeyCode.D))
-        // {
-        //     inputVector.x = 1;
-        // }
         inputVector.Normalize();
         return inputVector;
     }
--- Assets/_Game/Scripts/Player/PlayerMotor.cs
+++ Assets/_Game/Scripts/Player/PlayerMotor.cs
@@ -8,7 +8,8 @@ public class PlayerMotor : MonoBehaviour
 {
     [SerializeField] float _speed = 5;
     [SerializeField] float _rotSpeed = 5;
-
+    [SerializeField] [Min(.5f)] float _collisionDetectionRayMaxLength = .7f;
+    [SerializeField] [Min(.5f)] float _collisionDetectionSphereRadius = .7f;
     public event Action<bool> OnMove;
     InputHandler _inputHandler;
     Transform _tr;
@@ -32,10 +33,14 @@ public class PlayerMotor : MonoBehaviour
             OnMove?.Invoke(false);
             return;
         }
-        
+
         OnMove?.Invoke(true);
         
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        
+        if (!CanMove(ref moveDir)) return;
+        
+        moveDir.Normalize();
         
         Move(moveDir);
 
@@ -52,5 +57,45 @@ public class PlayerMotor : MonoBehaviour
         Vector3 rotDir = _tr.forward;
         rotDir = Vector3.Slerp(rotDir, moveDir, Time.deltaTime * _rotSpeed);
         _tr.forward = rotDir;
+    }
+
+    bool CanMove(ref Vector3 moveDir)
+    {
+        bool canMove = false;
+        canMove = !Physics.SphereCast(_tr.position, _collisionDetectionSphereRadius, moveDir, out RaycastHit hitInfo,
+            _collisionDetectionRayMaxLength);
+        if (canMove) return true;
+
+        if (!Mathf.Approximately(moveDir.x, 0))
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            Debug.Log("red");
+            Debug.DrawLine(_tr.position, _tr.position + moveDirX, Color.red);
+            canMove = !Physics.SphereCast(_tr.position, _collisionDetectionSphereRadius, moveDirX, out RaycastHit hitInfo2,
+                _collisionDetectionRayMaxLength);
+            if(canMove) moveDir = moveDirX;
+            
+        }
+
+        if (canMove) return true;
+        
+        if (!Mathf.Approximately(moveDir.z, 0))
+        {
+            Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+            Debug.DrawLine(_tr.position, _tr.position + moveDirZ, Color.green);
+            canMove = !Physics.SphereCast(_tr.position, _collisionDetectionSphereRadius, moveDirZ, out RaycastHit hitInfo2,
+                _collisionDetectionRayMaxLength);
+            if(canMove) moveDir = moveDirZ;
+        }
+        
+        return canMove;
+    }
+
+    void OnDrawGizmos()
+    {
+        // draw collision detection sphere        
+        _tr = transform;
+        Gizmos.color = Color.red;
+        // Gizmos.DrawSphere(_tr.position, _collisionDetectionSphereRadius);
     }
 }
