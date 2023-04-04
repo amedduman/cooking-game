f9b158327696d31be831fd18f4ca4c2cebfd13e5
amedduman <amedduman@protonmail.com>
2023-04-04 21:53:57 +0300

    Start working on delivery counter

---
+++ Assets/_Game/Scripts/Counter/DeliveryCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : Counter
+{
+    public override void Interact()
+    {
+        if (_player.MyKitchenObject == null) return;
+        if (_player.MyKitchenObject.IsPlate)
+        {
+            var returnedObj = _player.DropKitchenObject();
+            PutKitchenObjToPos(returnedObj);
+        }
+    }
+}
