d22e8bef530f3bd3f0a88e99d3d1be9eee9da3b3
amedduman <amedduman@protonmail.com>
2023-04-07 22:08:16 +0300

    Remove commented out code from DeliveryManager.cs

--- Assets/_Game/Scripts/DeliveryManager.cs
+++ Assets/_Game/Scripts/DeliveryManager.cs
@@ -10,7 +10,6 @@ public class DeliveryManager : MonoBehaviour
     [SerializeField] int _totalNumberOfOrdersToPrepare = 3;
     [SerializeField] int _timeToCompleteOrders = 10;
     int _givenOrderNumber;
-    //int _deliveredOrderCount;
     float _gameTimer;
 
     List<OrderInfo> _waitingOrders = new();
@@ -34,11 +33,6 @@ public class DeliveryManager : MonoBehaviour
             ServiceLocator.Get<GameTimerCanvas>().UpdateTheTimer(_timeToCompleteOrders - (int)_gameTimer);
             if ((int)_gameTimer >= _timeToCompleteOrders)
             {
-                //if (_deliveredOrderCount < _totalNumberOfOrdersToPrepare)
-                //{
-                //    Debug.Log("lose");
-                //}
-
                 if(_waitingOrders.Count > 0)
                 {
                     Debug.Log("lose");
@@ -55,10 +49,6 @@ public class DeliveryManager : MonoBehaviour
                 return (int)_gameTimer < _timeToCompleteOrders;
             }
 
-            //if (_deliveredOrderCount < _totalNumberOfOrdersToPrepare)
-            //{
-            //    return (int)_gameTimer < _timeToCompleteOrders;
-            //}
             return false;
         }
     }
@@ -127,19 +117,6 @@ public class DeliveryManager : MonoBehaviour
 
     public void OrderTrashed(Recipe recipe)
     {
-        //foreach (var order in _waitingOrders)
-        //{
-        //    if (order.IsBeingPrepared)
-        //    {
-        //        if (order.MyRecipe.RecipeName == recipe.RecipeName)
-        //        {
-        //            order.IsBeingPrepared = false;
-        //            _deliveryUI.UpdateWaitingOrderList(_waitingOrders);
-        //            break;
-        //        }
-        //    }
-        //}
-
         var order = GetFirstOrderInWaitingOrdersListWhichIsBeingPrepared(recipe);
         order.IsBeingPrepared = false;
         _deliveryUI.UpdateWaitingOrderList(_waitingOrders);
@@ -174,13 +151,7 @@ public class DeliveryManager : MonoBehaviour
             var order = GetFirstOrderInWaitingOrdersListWhichIsBeingPrepared(kitchenObj.MyRecipe);
             _waitingOrders.Remove(order);
             _deliveryUI.UpdateWaitingOrderList(_waitingOrders);
-            //_deliveredOrderCount++;
             Destroy(kitchenObj.gameObject);
-
-            //if (_deliveredOrderCount == _totalNumberOfOrdersToPrepare)
-            //{
-            //    ServiceLocator.Get<GameManager>().WinGame();
-            //}
 
             if (_waitingOrders.Count == 0)
             {
